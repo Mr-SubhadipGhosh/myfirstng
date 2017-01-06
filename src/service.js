8f8a6ec05ac7b645af7cb6acefbcd26b34f6a1d5
unknown <ghosh_su@Subhadip-G.India.XoriantCorp.com>
2017-01-07 03:50:41 +0530

    added mock data service

--- src/service.js
+++ src/service.js
@@ -1,3 +1,20 @@
-myapp.service('serv', function(){
+myapp.service('serv', function ($http, $q) {
     this.test = 'emp1';
+
+    this.getData = function () {
+        $http.get("MOCK_DATA.json")
+            .then(function (response) {
+                this.data = response.data;
+                console.log(this.data);
+            });
+    };
+    this.getDataPromis = function () {
+        var deferred = $q.defer();
+        $http.get('MOCK_DATA.json').then(function (data) {
+                deferred.resolve(data.data);
+            },function (msg, code) {
+                deferred.reject(msg);
+            });
+        return deferred.promise;
+    }
 });
